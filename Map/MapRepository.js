57f5962c798fdb21e3d8cd5842694ca1831f5b23
MakTak <justaway22@gmail.com>
2019-09-17 16:33:12 +0900

    もろもろ修正

--- Map/MapRepository.js
+++ Map/MapRepository.js
@@ -10,54 +10,53 @@ const DBName = config.DB.Name;
 const DBURL = config.DB.URL + "/" + DBName;
 
 module.exports = class MapRepository {
-    static async addMap(mapData) {
-        const newMap = new Map(mapData["name"], mapData["size"], mapData["mName"]);
-        const client = await MongoClient.connect(DBURL)
-            .catch((err) => {
-                console.log(err);
-            });
-        const db = client.db(DBName);
-        const res = await db.collection('map').insert(newMap);
-        const map = await db.collection('map').findOne({name: newMap.name});
-        client.close();
-        return map;
-    }
+  static async addMap(mapData) {
+    const newMap = new Map(mapData["name"], mapData["size"], mapData["mName"]);
+    const client = await MongoClient.connect(DBURL).catch(err => {
+      console.log(err);
+    });
+    const db = client.db(DBName);
+    const res = await db.collection("map").insert(newMap);
+    const map = await db.collection("map").findOne({ name: newMap.name });
+    client.close();
+    return map;
+  }
 
-    static async removeMap(searchedMapName) {
-        const client = await MongoClient.connect(DBURL)
-            .catch((err) => {
-                console.log(err);
-            });
-        const db = client.db(DBName);
-        const removeQuery = {name: searchedMapName};
-        const res = await db.collection('map').remove(removeQuery);
-        client.close();
-    }
+  static async removeMap(searchedMapName) {
+    const client = await MongoClient.connect(DBURL).catch(err => {
+      console.log(err);
+    });
+    const db = client.db(DBName);
+    const removeQuery = { name: searchedMapName };
+    const res = await db.collection("map").remove(removeQuery);
+    client.close();
+  }
 
-    static async getMap(searchedMapName) {
-        const client = await MongoClient.connect(DBURL)
-            .catch((err) => {
-                console.log(err);
-            });
-        const db = client.db(DBName);
-        const searchQuery = {name: searchedMapName};
-        const map = await db.collection('map').findOne(searchQuery);
-        client.close();
-        return map;
-    }
+  static async getMap(searchedMapName) {
+    const client = await MongoClient.connect(DBURL).catch(err => {
+      console.log(err);
+    });
+    const db = client.db(DBName);
+    const searchQuery = { name: searchedMapName };
+    const map = await db.collection("map").findOne(searchQuery);
+    client.close();
+    return map;
+  }
 
-    static async getAllMap() {
-        const client = await MongoClient.connect(DBURL)
-            .catch((err) => {
-                console.log(err);
-            });
-        const db = client.db(DBName);
-        const mapQuery = await db.collection('map').find().toArray();
-        client.close();
-        let maps = [];
-        for(let map of mapQuery) {
-            maps.push(map);
-        }
-        return maps;
+  static async getAllMap() {
+    const client = await MongoClient.connect(DBURL).catch(err => {
+      console.log(err);
+    });
+    const db = client.db(DBName);
+    const mapQuery = await db
+      .collection("map")
+      .find()
+      .toArray();
+    client.close();
+    let maps = [];
+    for (let map of mapQuery) {
+      maps.push(map);
     }
+    return maps;
+  }
 };
--- service/APIHandlers.js
+++ service/APIHandlers.js
@@ -1,171 +1,175 @@
-'use strict';
+"use strict";
 
-const PositionTracking = require('./PositionTracking');
-const DetectionDataRepository = require('../DetectionData/DetectionDataRepository');
-const TrackerRepository = require('../Tracker/TrackerRepository');
-const DetectorRepository = require('../Detector/DetectorRepository');
-const MapRepository = require('../Map/MapRepository');
-const MetaRepository = require('../Meta/MetaRepository');
-const Alert = require('../Alert/Alert');
+const PositionTracking = require("./PositionTracking");
+const DetectionDataRepository = require("../DetectionData/DetectionDataRepository");
+const TrackerRepository = require("../Tracker/TrackerRepository");
+const DetectorRepository = require("../Detector/DetectorRepository");
+const MapRepository = require("../Map/MapRepository");
+const MetaRepository = require("../Meta/MetaRepository");
+const Alert = require("../Alert/Alert");
+const { performance } = require("perf_hooks");
 let timerID;
 
 module.exports = class APIHandlers {
-    static addDetectionData(req, res) {
-        const detectionData = req.body;
-        DetectionDataRepository.addDetectionData(detectionData).then(() => {
-            res.send("DetectionData Add Success!");
-        });
-    }
+  static addDetectionData(req, res) {
+    const detectionData = req.body;
+    DetectionDataRepository.addDetectionData(detectionData).then(() => {
+      res.send("DetectionData Add Success!");
+    });
+  }
 
-    static addTracker(req, res) {
-        const tracker = req.body;
-        TrackerRepository.addTracker(tracker).then(() => {
-            res.send("Tracker Add Success!");
-        });
-    }
+  static addTracker(req, res) {
+    const tracker = req.body;
+    TrackerRepository.addTracker(tracker).then(() => {
+      res.send("Tracker Add Success!");
+    });
+  }
 
-    static addDetector(req, res) {
-        const detector = req.body;
-        DetectorRepository.addDetector(detector).then(() => {
-            res.send("Detector Add Success!");
-        });
-    }
+  static addDetector(req, res) {
+    const detector = req.body;
+    DetectorRepository.addDetector(detector).then(() => {
+      res.send("Detector Add Success!");
+    });
+  }
 
-    static updateDetectorActiveLastTime(req, res) {
-        const detector = req.body;
-        DetectorRepository.updateDetectorActiveLastTime(detector).then(() => {
-            res.send("Detector Active Time Updated!");
-        });
-    }
+  static updateDetectorActiveLastTime(req, res) {
+    const detector = req.body;
+    DetectorRepository.updateDetectorActiveLastTime(detector).then(() => {
+      res.send("Detector Active Time Updated!");
+    });
+  }
 
-    static addMap(req, res) {
-        const map = req.body;
-        MapRepository.addMap(map).then((map) => {
-            res.json(map);
-        });
-    }
+  static addMap(req, res) {
+    const map = req.body;
+    MapRepository.addMap(map).then(map => {
+      res.json(map);
+    });
+  }
 
-    static async getAllTracker(req, res) {
-        let searchTime = {};
-        if (req.query.start && req.query.end) {
-            searchTime = {
-            start: Number(req.query.start),
-            end: Number(req.query.end)
-            };
-        }
-        const trackers = await TrackerRepository.getAllTracker(searchTime);
-        res.json(trackers);
+  static async getAllTracker(req, res) {
+    let searchTime = {};
+    if (req.query.start && req.query.end) {
+      searchTime = {
+        start: Number(req.query.start),
+        end: Number(req.query.end)
+      };
     }
+    const checkStartTime = performance.now();
+    const trackers = await TrackerRepository.getAllTracker(searchTime);
+    const checkEndTime = performance.now();
+    console.log("EstimateTime: " + String(checkEndTime - checkStartTime));
+    res.json(trackers);
+  }
 
-    static searchTrackerByID(req, res) {
-        const trackerID = req.params.id;
-        let times = {};
-        if (req.query.start && req.query.end) {
-            times = {
-                start: Number(req.query.start),
-                end: Number(req.query.end)
-            };
-        }
-        TrackerRepository.getTrackerByTrackerID(trackerID, times).then(response => {
-            res.json(response);
-        });
+  static searchTrackerByID(req, res) {
+    const trackerID = req.params.id;
+    let times = {};
+    if (req.query.start && req.query.end) {
+      times = {
+        start: Number(req.query.start),
+        end: Number(req.query.end)
+      };
     }
+    TrackerRepository.getTrackerByTrackerID(trackerID, times).then(response => {
+      res.json(response);
+    });
+  }
 
-    static updateTrackerByID(req, res) {
-        const trackerID = req.params.id;
-        const newValueQuery = req.body;
-        TrackerRepository.updateTracker(trackerID, newValueQuery).then(() => {
-            res.send("Tracker Update Success!");
-        });
-    }
+  static updateTrackerByID(req, res) {
+    const trackerID = req.params.id;
+    const newValueQuery = req.body;
+    TrackerRepository.updateTracker(trackerID, newValueQuery).then(() => {
+      res.send("Tracker Update Success!");
+    });
+  }
 
-    static addTracker(req, res) {
-        const tracker = req.body;
-        TrackerRepository.addTracker(tracker).then(() => {
-            res.send("Tracker Add Success!");
-        });
-    }
+  static addTracker(req, res) {
+    const tracker = req.body;
+    TrackerRepository.addTracker(tracker).then(() => {
+      res.send("Tracker Add Success!");
+    });
+  }
 
-    static addTrackerMailAddr(req, res) {
-        const trackerID = req.params.id;
-        const addr = req.body;
-        TrackerRepository.addTrackerMailAddr(trackerID, addr).then(() => {
-            res.send("TrackerAddr Update Success!");
-        });
-    }
+  static addTrackerMailAddr(req, res) {
+    const trackerID = req.params.id;
+    const addr = req.body;
+    TrackerRepository.addTrackerMailAddr(trackerID, addr).then(() => {
+      res.send("TrackerAddr Update Success!");
+    });
+  }
 
-    static startPositionTracking(req, res) {
-        timerID = setInterval(() => {
-            const date = new Date();
-            const startTime = date.getTime()-1000;
-            PositionTracking.updateLocations(startTime);
-            Alert.check();
-        }, 1000);
-        res.send("Tracking Start!");
-    }
+  static startPositionTracking(req, res) {
+    timerID = setInterval(() => {
+      const date = new Date();
+      const startTime = date.getTime() - 1000;
+      PositionTracking.updateLocations(startTime);
+      Alert.check();
+    }, 1000);
+    res.send("Tracking Start!");
+  }
 
-    static stopPositionTracking(req, res) {
-        clearInterval(timerID);
-        res.send("Tracking Stop!");
-    }
+  static stopPositionTracking(req, res) {
+    clearInterval(timerID);
+    res.send("Tracking Stop!");
+  }
 
-    static getDetector(req, res) {
-        DetectorRepository.getDetector().then((detector) => {
-            res.send(detector);
-        });
-    }
+  static getDetector(req, res) {
+    DetectorRepository.getDetector().then(detector => {
+      res.send(detector);
+    });
+  }
 
-    static deleteDetector(req, res) {
-        const detectorId = req.body.detectorNumber;
-        DetectorRepository.removeDetector(detectorId).then(() => {
-            res.send('Successfully delete detector!');
-        });
-    }
+  static deleteDetector(req, res) {
+    const detectorId = req.body.detectorNumber;
+    DetectorRepository.removeDetector(detectorId).then(() => {
+      res.send("Successfully delete detector!");
+    });
+  }
 
-    static putDetector(req, res) {
-        const detector = req.body;
-        DetectorRepository.updateDetector(detector).then(() => {
-            res.send('Successfully put detector!');
-        });
-    }
+  static putDetector(req, res) {
+    const detector = req.body;
+    DetectorRepository.updateDetector(detector).then(() => {
+      res.send("Successfully put detector!");
+    });
+  }
 
-    static getMap(req, res) {
-        MapRepository.getAllMap().then((map) => {
-            res.send(map);
-        });
-    }
+  static getMap(req, res) {
+    MapRepository.getAllMap().then(map => {
+      res.send(map);
+    });
+  }
 
-    static deleteMap(req, res) {
-        const mapId = req.params.id;
-        MapRepository.removeMap(mapId).then(() => {
-            res.send('Successfully delete map!');
-        });
-    }
+  static deleteMap(req, res) {
+    const mapId = req.params.id;
+    MapRepository.removeMap(mapId).then(() => {
+      res.send("Successfully delete map!");
+    });
+  }
 
-    static addMeta(req, res) {
-        const meta = req.body;
-        MetaRepository.addMeta(meta).then(() => {
-            res.send("Meta Add Success!");
-        });
-    }
+  static addMeta(req, res) {
+    const meta = req.body;
+    MetaRepository.addMeta(meta).then(() => {
+      res.send("Meta Add Success!");
+    });
+  }
 
-    static getMeta(req, res) {
-        MetaRepository.getAllMeta().then((meta) => {
-            res.send(meta);
-        });
-    }
+  static getMeta(req, res) {
+    MetaRepository.getAllMeta().then(meta => {
+      res.send(meta);
+    });
+  }
 
-    static deleteMeta(req, res) {
-        const metaId = req.params.id;
-        MetaRepository.removeMeta(metaId).then(() => {
-            res.send('Successfully delete meta!');
-        });
-    }
+  static deleteMeta(req, res) {
+    const metaId = req.params.id;
+    MetaRepository.removeMeta(metaId).then(() => {
+      res.send("Successfully delete meta!");
+    });
+  }
 
-    static putMeta(req, res) {
-        const meta = req.body;
-        MetaRepository.updateMeta(meta).then(() => {
-            res.send('Successfully put meta!');
-        });
-    }
+  static putMeta(req, res) {
+    const meta = req.body;
+    MetaRepository.updateMeta(meta).then(() => {
+      res.send("Successfully put meta!");
+    });
+  }
 };
