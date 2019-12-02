4b3accc6ee7e174900f3149ae0721578e84ac4c4
dani0678 <b16t2070c@edu.tottori-u.ac.jp>
2019-12-02 12:15:10 +0900

    ひもづけまで

--- webapp/src/Setting/Schedule/List/List.js
+++ webapp/src/Setting/Schedule/List/List.js
@@ -17,6 +17,8 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import ListIcon from '@material-ui/icons/List';
 import IconButton from '@material-ui/core/IconButton';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
 import Dialog from '@material-ui/core/Dialog';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -32,29 +34,8 @@ const useStyles = makeStyles({
     overflow: 'auto',
   },
 });
-
-const useToolbarStyles = makeStyles(theme => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-    theme.palette.type === 'light'
-      ? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-        }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
-  title: {
-    flex: '1 1 100%',
-  },
-}));
-
+//タイトル（ツールバー）作成
 const EnhancedTableToolbar = props => {
-  const classes = useToolbarStyles();
   const { selected } = props;
   const { deleteSchedule } = props;
 
@@ -92,49 +73,68 @@ EnhancedTableToolbar.propTypes = {
   selected: PropTypes.array.isRequired,
   deleteSchedule: PropTypes.func.isRequired,
 };
-
-function SelectDialog(props) {
+//dialog作成
+function SimpleDialog(props) {
   const { onClose, open, residentsList, trackerIDList, updateTrackerList, scheduleID } = props;
+  const [selected, setSelected] = React.useState(trackerIDList);
 
   const handleClose = () => {
     onClose();
+    setSelected(trackerIDList);
   };
 
-  const handleListItemClick = value => {
-    const IDListCopy = trackerIDList;
-    if (IDListCopy) {
-      if (IDListCopy.find(id => id === value.trackerID)) {
-        const newIDList = IDListCopy.filter(id => id != value.trackerID);
-        updateTrackerList(scheduleID, newIDList);
-      } else {
-        IDListCopy.push(value.trackerID);
-        updateTrackerList(scheduleID, IDListCopy);
-      }
+  const submitList = () => {
+    updateTrackerList(scheduleID, selected);
+  };
+
+  const handleClick = trackerID => {
+    const selectedIndex = selected.indexOf(trackerID);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, trackerID);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
     }
+    setSelected(newSelected);
   };
 
   return (
     <Dialog onClose={handleClose} aria-labelledby="tracker-dialog" open={open}>
       <DialogTitle id="tracker-dialog">入居者を選んでください</DialogTitle>
-      <List>
-        {residentsList.map(tracker => (
-          <ListItem key={tracker.trackerName}>
-            <Checkbox
-              onClick={() => handleListItemClick(tracker)}
-              checked={trackerIDList.includes(tracker.trackerID)}
-            />
-            <ListItemText primary={tracker.trackerName} />
-          </ListItem>
-        ))}
-      </List>
-      <Button variant="contained" color="primary" size="medium" onClick={handleClose}>
-        閉じる
-      </Button>
+      <DialogContent>
+        <List>
+          {residentsList.map(tracker => (
+            <ListItem key={tracker.trackerName}>
+              <Checkbox
+                onClick={() => handleClick(tracker.trackerID)}
+                checked={selected.includes(tracker.trackerID)}
+              />
+              <ListItemText primary={tracker.trackerName} />
+            </ListItem>
+          ))}
+        </List>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" color="primary" size="small" onClick={submitList}>
+          登録
+        </Button>
+        <Button variant="contained" color="primary" size="small" onClick={handleClose}>
+          閉じる
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
 
-SelectDialog.propTypes = {
+SimpleDialog.propTypes = {
   onClose: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
 };
@@ -154,12 +154,13 @@ export default function ScheduleList(props) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [selected, setSelected] = React.useState([]);
-  const [open, setOpen] = React.useState(false);
+  const [openId, setOpenId] = React.useState(null);
 
   const residentsList = props.trackerList.filter(function(tracker) {
     if (tracker.userStatus === '入居者') return true;
   });
-  //Rows per page
+
+  //tableのページとcheckbox
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -168,7 +169,7 @@ export default function ScheduleList(props) {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-  //handleCheckbox
+
   const handleClick = (event, name) => {
     const selectedIndex = selected.indexOf(name);
     let newSelected = [];
@@ -189,17 +190,17 @@ export default function ScheduleList(props) {
   };
 
   const isSelected = name => selected.indexOf(name) !== -1;
-  //handleDialog
-  const handleClickOpen = () => {
-    setOpen(true);
+  //dialog
+  const handleClickOpen = id => {
+    setOpenId(id);
   };
 
-  const handleClose = value => {
-    setOpen(false);
+  const handleClose = () => {
+    setOpenId(null);
   };
   //API
   const updateTrackerList = (shceduleID, trackerList) => {
-    const scheduleURL = `${process.env.REACT_APP_API_URL}/api/schedule`;
+    const scheduleURL = 'http://localhost:3000/api/schedule';
     const updateScheduleUrl = scheduleURL + '/' + shceduleID;
     fetch(updateScheduleUrl, {
       method: 'PUT',
@@ -209,7 +210,7 @@ export default function ScheduleList(props) {
   };
 
   const deleteSchedule = selected => {
-    const scheduleURL = `${process.env.REACT_APP_API_URL}/api/schedule`;
+    const scheduleURL = 'http://localhost:3000/api/schedule';
     selected.map(select => {
       const schedule = props.scheduleList.find(schedule => schedule.name === select);
       const deleteScheduleUrl = scheduleURL + '/' + schedule.scheduleID;
@@ -221,7 +222,7 @@ export default function ScheduleList(props) {
       setSelected([]);
     });
   };
-
+  //Table作成
   const makeScheduleList = schedules => {
     schedules = _.sortBy(schedules, schedule => {
       return schedule.openingTime;
@@ -250,18 +251,23 @@ export default function ScheduleList(props) {
               <TableCell align="center">{unitName}</TableCell>
               <TableCell align="center">{roomName}</TableCell>
               <TableCell>
-                <IconButton onClick={handleClickOpen}>
+                <IconButton
+                  onClick={() => {
+                    handleClickOpen(schedule.scheduleID);
+                  }}
+                >
                   <ListIcon />
                 </IconButton>
+
+                <SimpleDialog
+                  open={openId === schedule.scheduleID}
+                  onClose={handleClose}
+                  residentsList={residentsList}
+                  trackerIDList={schedule.trackerList}
+                  updateTrackerList={updateTrackerList}
+                  scheduleID={schedule.scheduleID}
+                />
               </TableCell>
-              <SelectDialog
-                open={open}
-                onClose={handleClose}
-                residentsList={residentsList}
-                trackerIDList={schedule.trackerList}
-                updateTrackerList={updateTrackerList}
-                scheduleID={schedule.scheduleID}
-              />
             </TableRow>
           );
         }
